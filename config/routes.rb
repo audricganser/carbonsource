944e5ebd3d4ab134571841f51488f61c47f6490b
LMCMLJ <mjcalvey2@gmail.com>
2016-12-12 21:48:35 +0000

    Updates routes so that urls are shorter and more restful

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/' => "search#index"
+  get '/' => "search#new"
 
-  get 'search/new' => "search#new"
+  post 'search/create' => "search#create"
 
-  post 'search/create'
+  get 'search/:id' => "search#show"
 
-  get 'search/show' => "search#show"
-
-  get 'search/list' => "search#list"
+  get 'search/' => "search#list"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
