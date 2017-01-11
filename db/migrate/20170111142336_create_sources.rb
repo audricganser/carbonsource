2e207e7ab3e25d7c3dd81e5ef6d0b1cd7d572777
LMCMLJ <mjcalvey2@gmail.com>
2017-01-11 14:25:38 +0000

    Adds source migration

--- db/migrate/20170111142336_create_sources.rb
+++ db/migrate/20170111142336_create_sources.rb
@@ -1,6 +1,10 @@
 class CreateSources < ActiveRecord::Migration[5.0]
   def change
     create_table :sources do |t|
+      t.string :name
+      t.string :description
+      t.string :url
+      t.string :remark
     end
   end
 end
